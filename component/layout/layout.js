9572f9074a17ece4e877bd4c76d2e5492505b355
b.niazi <b.niazi@kintechmail.com>
2023-02-27 23:24:27 +0330

    remove horizentall scroll from site and add error page

--- component/layout/layout.js
+++ component/layout/layout.js
@@ -3,7 +3,7 @@ import Header from "../header/header";
 
 export default function Layout(props) {
   return (
-    <div>
+    <div className="w-full overflow-x-hidden">
       <Header />
       {props.children}
       <Footer />
