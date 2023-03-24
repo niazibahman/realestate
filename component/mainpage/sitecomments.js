ec08a523d050ab610deb98a47c395d2e31f233df
b.niazi <niazi.2021.b@gmail.com>
2023-03-25 00:34:32 +0330

    fix slider comment in comment section of main page & add neighbourhood data in database file

--- component/mainpage/sitecomments.js
+++ component/mainpage/sitecomments.js
@@ -22,15 +22,27 @@ export default function SiteComments(){
     return(
     <section className="w-full h-auto bg-commentBackground relative">
         <div className="w-full h-auto sm:container flex flex-col md:flex-row items-center md:justify-between px-6  sm:px-0 mx-auto text-commentText pt-10">
-            <div className="w-full md:w-1/2 flex flex-col items-start mb-4">
+            <div className="w-full overflow-x-hidden md:w-1/2 flex flex-col items-start mb-4">
                 <h2 className="font-bold text-3xl">نظرات مردم درباره ریکادا</h2>
-                <div className={`w-2/3 flex flex-col items-start transition-all enterIn`}>
-                    <p className="my-4">{comments[currentContent].comment}</p>
-                    <div className="w-full flex flex-row items-center">
-                        <Image src={comments[currentContent].img} alt="" width={85} height={85} className="rounded-full border-8 border-white border-opacity-30"/>
-                        <div className="flex flex-col mr-4">
-                            <h4 className="font-semibold text-xl mb-2">{comments[currentContent].name}</h4>
-                            <span>{comments[currentContent].job}</span>
+                <div className="w-full h-48 relative">
+                    <div className={`w-full absolute top-0 flex flex-col items-start transition-all ${currentContent===0?"enterIn":"exitOut"}`}>
+                        <p className="my-4">{comments[0].comment}</p>
+                        <div className="w-full flex flex-row items-center">
+                            <Image src={comments[0].img} alt="" width={85} height={85} className="rounded-full border-8 border-white border-opacity-30"/>
+                            <div className="flex flex-col mr-4">
+                                <h4 className="font-semibold text-xl mb-2">{comments[0].name}</h4>
+                                <span>{comments[0].job}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`w-full absolute top-0 flex flex-col items-start transition-all ${currentContent===1?"enterIn":"exitOut"}`}>
+                        <p className="my-4">{comments[1].comment}</p>
+                        <div className="w-full flex flex-row items-center">
+                            <Image src={comments[1].img} alt="" width={85} height={85} className="rounded-full border-8 border-white border-opacity-30"/>
+                            <div className="flex flex-col mr-4">
+                                <h4 className="font-semibold text-xl mb-2">{comments[1].name}</h4>
+                                <span>{comments[1].job}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
