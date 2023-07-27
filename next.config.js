cedd63ef16249d1c30c989cf919968a96baefbac
b.niazi <niazi.2021.b@gmail.com>
2023-07-27 14:46:03 +0330

    add new pic for ads

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+// import nextPwa from 'next-pwa';
+
+// const withPWA = nextPwa({
+//   dest:'public',
+//   register: true,
+//  });
+ 
 const nextConfig = {
   reactStrictMode: true,
 }
