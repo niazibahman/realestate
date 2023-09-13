9d3e116253d16b30b85f82a2969dacb355f200ab
b.niazi <niazi.2021.b@gmail.com>
2023-09-14 01:17:03 +0330

    add detail page and hover to slug and image in listing page

--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Flicking from "@egjs/react-flicking";
+import "@egjs/react-flicking/dist/flicking.css";
 
 export async function getServerSideProps(context) {
     return {
@@ -8,12 +10,28 @@ export async function getServerSideProps(context) {
 }
 
 export default function Detail(){
+
     return(
     <>
     <Head>
         <title>جزئیات آگهی</title>
     </Head>
-    <main>bahman</main>
+    <main>
+        <section className="w-screen grid grid-cols-12 gap-0">
+            <div className="col-span-full md:col-span-7 md:col-start-6">
+            <Flicking resizeOnContentsReady={false} renderOnSameKey={false} align="center" horizontal={true} circular={true}>
+              <img src="/p-1.webp" alt="1" className="w-full h-full object-cover"/>
+              <img src="/p-2.webp" alt="1" className="w-full h-full object-cover"/>
+              <img src="/p-3.webp" alt="1" className="w-full h-full object-cover"/>
+              <img src="/p-4.webp" alt="1" className="w-full h-full object-cover"/>
+              <img src="/p-5.webp" alt="1" className="w-full h-full object-cover"/>
+              <img src="/p-6.webp" alt="1" className="w-full h-full object-cover"/>
+            </Flicking>
+            </div>
+            <div className="col-span-full md:col-span-5 md:row-start-1"></div>
+            <div className="col-span-full md:col-span-full"></div>
+        </section>
+    </main>
     </>
     );
 }
